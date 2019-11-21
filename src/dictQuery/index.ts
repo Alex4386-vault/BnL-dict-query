5aa51e294a4ce1bd487969f8e7df4ac275d9ce25
catsaveearth <tpfbdpf@naver.com>
2019-11-21 18:25:54 +0900

    fix: bug report by ms.queen

--- src/dictQuery/index.ts
+++ src/dictQuery/index.ts
@@ -15,73 +15,79 @@ interface Definition {
     sub: string[];
 }
 
-export function dictionaryQuery(word){
+export function dictionaryQuery(word): Promise<Word> {
+  return new Promise<Word> (
+    (resolve, reject) => {
+      const option = {
+          url: "https://www.merriam-webster.com/dictionary/"+encodeURIComponent(word),
+          headers: {
+              "User-Agent": userAgentString
+          }
+      }
+      request(option, (err, response, body) => {
+          if (!err && response.statusCode === 200) {
+              const { document } = new JSDOM(body).window;
+              const defWrap = document.getElementById("definition-wrapper");
+              const wordHeaders = defWrap.getElementsByClassName("anchor-name");
+              const lengthWords = wordHeaders.length;
 
-    const option = {
-        url: "https://www.merriam-webster.com/dictionary/"+encodeURIComponent(word),
-        headers: {
-            "User-Agent": userAgentString
-        }
+              console.log(lengthWords);
+
+              for (let i = 0; i < lengthWords; i++) {
+                  const current = defWrap.getElementsByClassName("entry-header")[i];
+                  const theWord = stripHtmlTags(current.getElementsByClassName("hword")[0].innerHTML);
+                  let gerund = stripHtmlTags(current.getElementsByClassName("fl")[0].innerHTML);
+                  try {
+                      gerund += stripHtmlTags(current.getElementsByClassName("lbs")[0].innerHTML);
+                  } catch(e) {
+                  }
+
+                  console.log(theWord, gerund);
+
+                  const dictEntries = Array.from(defWrap.getElementsByClassName("vg")[0].getElementsByClassName("sb"));
+                  const definitions = [];
+                  for (let dictEntry of dictEntries) {
+                      let entryMainDefinition = undefined;
+                      let entrySubDefinitions = [];
+                      for (let meaningEntry of Array.from(dictEntry.getElementsByClassName("sense"))) {
+                          for (let examples of Array.from(meaningEntry.getElementsByClassName("ex-sent"))) {
+                              examples.remove();
+                          }
+
+                          let o = stripHtmlTags(meaningEntry.getElementsByClassName("dtText")[0].innerHTML).replace(/(\t|\n)/g,"").trim()
+                          if (meaningEntry.getElementsByClassName("sl").length != 0) {
+                              o = stripHtmlTags(meaningEntry.getElementsByClassName("sl")[0].innerHTML) + o;
+                          } else {
+                              if (/^( )+$/.test(o)) { continue; }
+                          }
+                          o = o.replace(/^: /, "");
+                          if (meaningEntry.classList.contains("has-num-only")) {
+                              entryMainDefinition = o;
+                          } else {
+                              entrySubDefinitions.push(o);
+                          }
+
+                      }
+                      definitions.push({
+                          main: entryMainDefinition,
+                          sub: entrySubDefinitions
+                      } as Definition);
+                  }
+
+
+                  const word: Word = {
+                      word: theWord,
+                      definitions
+                  };
+
+                  resolve(word);
+              }
+          }
+      });
     }
-    request(option, (err, response, body) => {
-        if (!err && response.statusCode === 200) {
-            const { document } = new JSDOM(body).window;
-            const defWrap = document.getElementById("definition-wrapper");
-            const wordHeaders = defWrap.getElementsByClassName("anchor-name");
-            const lengthWords = wordHeaders.length;
-
-            console.log(lengthWords);
-
-            for (let i = 0; i < lengthWords; i++) {
-                const current = defWrap.getElementsByClassName("entry-header")[i];
-                const theWord = stripHtmlTags(current.getElementsByClassName("hword")[0].innerHTML);
-                let gerund = stripHtmlTags(current.getElementsByClassName("fl")[0].innerHTML);
-                try {
-                    gerund += stripHtmlTags(current.getElementsByClassName("lbs")[0].innerHTML);
-                } catch(e) {}
-
-                console.log(theWord, gerund);
-
-                const dictEntries = Array.from(defWrap.getElementsByClassName("vg")[0].getElementsByClassName("sb"));
-                const definitions = [];
-                for (let dictEntry of dictEntries) {
-                    let entryMainDefinition = undefined;
-                    let entrySubDefinitions = [];
-                    for (let meaningEntry of Array.from(dictEntry.getElementsByClassName("sense"))) {
-                        for (let examples of Array.from(meaningEntry.getElementsByClassName("ex-sent"))) {
-                            examples.remove();
-                        }
-
-                        let o = stripHtmlTags(meaningEntry.getElementsByClassName("dtText")[0].innerHTML).replace(/(\t|\n)/g,"").trim()
-                        if (meaningEntry.getElementsByClassName("sl").length != 0) {
-                            o = stripHtmlTags(meaningEntry.getElementsByClassName("sl")[0].innerHTML) + o;
-                        } else {
-                            if (/^( )+$/.test(o)) { continue; }
-                        }
-                        o = o.replace(/^: /, "");
-                        if (meaningEntry.classList.contains("has-num-only")) {
-                            entryMainDefinition = o;
-                        } else {
-                            entrySubDefinitions.push(o);
-                        }
-                        
-                    }
-                    definitions.push({
-                        main: entryMainDefinition,
-                        sub: entrySubDefinitions
-                    } as Definition);
-                }
+  )
 
 
-                const word: Word = {
-                    word: theWord,
-                    definitions
-                };
-
-                console.log(util.inspect(word, false, null, true));
-            }
-        }
-    });
 
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import fs from "fs";
 import { dictionaryQuery } from "./dictQuery";
-//import { ConfigInterface } from "./interface/configInterface";
+import { ConfigInterface } from "./interface/configInterface";
+import util from "util";
 
-//const query = JSON.parse(fs.readFileSync("query/query.json").toString()) as ConfigInterface;
-/*
+const query = JSON.parse(fs.readFileSync("query/query.json").toString()) as ConfigInterface;
+
 if (query.sortMe) {
     query.queries.sort();
     console.log("Sorted!");
 }
 
 console.log(query.queries);
-*/
-dictionaryQuery("face");
-// dictionaryQuery(query.queries[0]);
 
-/*
-query.queries.forEach( (currentWord) => {
+let i = 0;
 
-})
-*/
+for (let i = 0; i < query.queries.length; i++) {
+  setTimeout( async () => {
+    const word = await dictionaryQuery(query.queries[i]);
+    console.log(util.inspect(word, false, null, true));
+  }, 1000*i);
+
+}
